4378fd0c057b7910c7812021c5644da66b3beef1
andy yee <andy.sksea@gmail.com>
2017-09-07 10:17:27 -0400

    Add webpack-dev-server and SE auth

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './lib/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  }
+};
